f5c993a7d9d3bc119023169c06c21a6d24d92434
Darrel2018 <darrelp2000@gmail.com>
2026-05-21 10:52:31 +0200

    Completed chapter 4 to 7

---
+++ my-app2/app/about/contact/page.jsx
@@ -0,0 +1,16 @@
+
+export const metadata = {
+  title: 'Contact Greg\'s Portfolio of Small Bets',
+  description: 'Tech Courses and Books',
+  keywords: 'passive income, small bets, tech courses, tech books, tech tutorials',
+};
+
+const ContactPage = () => {
+    return (
+        <div> 
+            <h1>Contact Page</h1> 
+            <p>Phone: 555-555-5555</p> 
+        </div> );
+}
+
+export default ContactPage;
